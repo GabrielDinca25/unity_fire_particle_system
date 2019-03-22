1f4b720022863e0c28772d06054fb2f9933b76cd
GabrielDinca25 <gdincagabriel@gmail.com>
2019-03-22 08:58:53 +0200

    Added fire, character and character controller

---
+++ Focuri/Focuri/Assets/RotateAround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public Transform Fire;
+    private Vector3 pos;
+
+    void FixedUpdate()
+    {
+        //float angle = Mathf.Atan2(transform.position.y, transform.position.x) * Mathf.Rad2Deg;
+        //if (angle < 0) angle += 360.0f;
+        ////transform.localEulerAngles = new Vector3(0, 0, angle);
+
+        //float xPos = -Mathf.Cos(Mathf.Deg2Rad * angle);
+        //float yPos = -Mathf.Sin(Mathf.Deg2Rad * angle);
+        //transform.localPosition = new Vector3(Fire.transform.position.x + xPos, Fire.transform.position.y + yPos + 0.5f, 0);
+        transform.RotateAround(Vector3.zero, Vector3.up, 10f * Time.fixedDeltaTime);
+    }
+}
---
+++ Focuri/Focuri/Assets/SP_Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SP_Movement : MonoBehaviour
+{
+    public float speed;
+    public float rotSpeed = 80;
+
+
+    private Rigidbody rb;
+    public CharacterController cc;
+    public Animation anim;
+    public AnimationClip Walk;
+    public AnimationClip Idle;
+    public AnimationClip running;
+
+    private bool once;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, rb.velocity.y, moveVertical);
+        Debug.Log(movement);
+
+
+        if (Input.GetKey(KeyCode.LeftShift) && (movement.z != 0 || movement.x != 0))
+        {
+            if (!once)
+            {
+                anim.clip = running;
+                anim.Play();
+                once = true;
+                speed = 4;
+            }
+        }
+        else
+        {
+            Debug.Log("m");
+            once = false;
+            if (movement.z != 0 || movement.x != 0)
+            {
+                anim.clip = Walk;
+                anim.Play();
+            }
+            else
+            {
+                anim.clip = Idle;
+                anim.Play();
+            }
+            speed = 2;
+        }
+        cc.Move(movement * Time.deltaTime * speed);
+        
+    }
+}
